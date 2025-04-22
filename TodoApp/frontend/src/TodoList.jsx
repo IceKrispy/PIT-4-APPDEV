1d9bb0c15074343e62172cf1f54e747f71efef79
Godwin Luna-an <160304412+IceKrispy@users.noreply.github.com>
2025-04-22 15:21:25 +0800

    todolist.jsx update

--- TodoApp/frontend/src/TodoList.jsx
+++ TodoApp/frontend/src/TodoList.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import "./index.css";
 
-const API_URL = "https://appdev-pit4.onrender.com/todos";
+const API_URL = "https://pit-4-appdev.onrender.com/";
 
 export default function TodoList() {
   const [tasks, setTasks] = useState([]);
